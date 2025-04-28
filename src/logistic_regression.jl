e6f8993d0bec35ffbb9403901141c7d9052b3393
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-04-28 11:53:20 +0100

    General Ralativistic HMC  (#4)
    * research: gr-hmc
    
    * feat: use PCA to perform dim red
    
    * feat: add flag for PCA or not
    
    * Add compat and remove outdated comments
    
    ---------
    
    Co-authored-by: Kai Xu <me@xuk.ai>
    Co-authored-by: Kai Xu <xuk@ibm.com>
    Co-authored-by: Qingyu Qu <2283984853@qq.com>

--- src/logistic_regression.jl
+++ src/logistic_regression.jl
@@ -1,26 +1,37 @@
+using MultivariateStats: MultivariateStats
+
+function reduce_dim_by_pca(design_matrix, target_dim::Int)
+    M = fit(MultivariateStats.PCA, design_matrix'; maxoutdim=target_dim)
+    return Matrix(MultivariateStats.predict(M, design_matrix')')
+end
+
 struct LogisticRegression{TX, Ty, T}
     X::TX
     y::Ty
     lambda::T
 end
 
-function LogisticRegression(datadir::String, lambda::AbstractFloat)
+function LogisticRegression(
+    datadir::String, lambda::AbstractFloat; 
+    num_obs::Int=1_000, num_latent::Int=300, no_pca::Bool=false
+)
     (; design_matrix, response) = BSON.load(joinpath(datadir, "germancredit.bson"))
-    return LogisticRegression(design_matrix', response, lambda)
+    design_matrix = size(design_matrix, 2) == num_latent ? design_matrix :
+        (no_pca ? design_matrix[:,1:num_latent] : reduce_dim_by_pca(design_matrix, num_latent))
+    return LogisticRegression(design_matrix[1:num_obs,:], Bool.(response[1:num_obs]), lambda)
 end
 
-LogisticRegression(lambda::AbstractFloat) = 
-    LogisticRegression(joinpath(splitdir(splitdir(pathof(@__MODULE__))[1])[1], "data"), lambda)
+LogisticRegression(lambda::AbstractFloat; kwargs...) = 
+    LogisticRegression(joinpath(splitdir(splitdir(pathof(@__MODULE__))[1])[1], "data"), lambda; kwargs...)
 
-dim(lr::LogisticRegression) = size(lr.X, 1) + 2
+dim(lr::LogisticRegression) = size(lr.X, 2) + 2
 
 function _logpdf(lr::LogisticRegression, theta)
-    n_chains = size(theta, 2)
-    logv = theta[1,:]
+    logv = theta[1,:]   # n_chains = size(theta, 2)
     v = exp.(logv)      # n_chains
     a = theta[2,:]      # n_chains
     b = theta[3:end,:]  # 300 x n_chains
-    logitp = a' .+ lr.X' * b
+    logitp = a' .+ lr.X * b
     p = logistic.(logitp)
     p = p * (1 - 2 * eps()) .+ eps()    # numerical stability
 
@@ -36,10 +47,26 @@ function _logpdf(lr::LogisticRegression, theta)
     return logprior + loglike
 end
 
-function logpdf(lr::LogisticRegression, theta::AbstractVector)
-    theta = reshape(theta, length(theta), 1)
-    lp = _logpdf(lr, theta)
-    return only(lp)
+_logpdf_exponential(x, lambda) = log(lambda) - lambda * x 
+_logpdf_bernoulli(x, p) = x * log(p) + (1 - x) * log(1 - p)
+
+# TODO extact out some functions and define @rule for them
+function _logpdf(lr::LogisticRegression, theta::AbstractVector{T}) where {T}
+    logv = @view theta[1:1]
+    v = exp.(logv)    # 1
+    a = @view theta[2:2]    # 1
+    b = @view theta[3:end]  # 300
+    # lr.X: 1000 x 300
+    logitp = a .+ lr.X * b # 1000 
+    p = logistic.(logitp) 
+    p = p * (1 - 2 * eps(T)) .+ eps(T)    # numerical stability
+
+    logabsdetjacob = logv
+    logprior = sum(_logpdf_exponential.(v, lr.lambda) + logabsdetjacob)
+    logprior += sum(_logpdf_normal_var.(a, 0, v))
+    logprior += sum(_logpdf_normal_var.(b, 0, v))
+    loglike = sum(_logpdf_bernoulli.(lr.y, p))
+    return logprior + loglike
 end
 
-logpdf(lr::LogisticRegression, theta::AbstractMatrix) = _logpdf(lr, theta)
+logpdf(lr::LogisticRegression, theta::AbstractVecOrMat) = _logpdf(lr, theta)
